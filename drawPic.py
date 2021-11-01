6acfb2667c9b3ac69852e5976507123fff988640
ubuntu <ubuntu@localhost.localdomain>
2021-11-01 22:46:35 +0800

    1

--- drawPic.py
+++ drawPic.py
@@ -1,6 +1,7 @@
 import matplotlib
 import matplotlib.pyplot as plt
 import os
+from pathlib import Path
 
 matplotlib.use('Agg')
 
@@ -14,16 +15,19 @@ def drawLine(y):
     plt.legend()
     plt.xlabel('num')
     plt.ylabel('price')
-    plt.savefig('./static/images/draw/line.png')
-    return
+    try:
+        plt.savefig(Path('./static/images/draw/line.png'))
+    except Exception as e:
+        print(e)
+    plt.close()
 
 
 def drawHist(y):
     if os.path.exists('./static/images/draw/hist.png'):
         os.remove('./static/images/draw/hist.png')
-    plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
-    plt.rcParams['axes.unicode_minus'] = False  # 这两行需要手动设置
-    # 添加图形属性
+    # plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
+    # plt.rcParams['axes.unicode_minus'] = False  # 这两行需要手动设置
+    # # 添加图形属性
 
     plt.xlabel('序号')
     plt.ylabel('价格')
@@ -43,4 +47,40 @@ def drawHist(y):
         plt.text(x + 0.15, y, str(y), va='bottom')  # 0.15为偏移值，可以自己调整，正好在柱形图顶部正中
 
     # 图片的显示及存储
-    plt.savefig("./static/images/draw/hist.png")
+    try:
+        plt.savefig(Path("./static/images/draw/hist.png"))
+    except Exception as e:
+        print(e)
+    plt.close()
+
+
+if __name__ == '__main__':
+    def test():
+        # plt.rcParams['font.sans-serif'] = ['SimHei']  # 显示中文标签
+        plt.rcParams['axes.unicode_minus'] = False  # 这两行需要手动设置
+        # 添加图形属性
+
+        plt.xlabel('序号')
+        plt.ylabel('价格')
+        plt.title('商品价格直方图')
+        y = [i for i in range(10)]
+        first_bar = plt.bar(range(len(y)), y, color='blue')  # 初版柱形图，x轴0-9，y轴是列表y的数据，颜色是蓝色
+
+        # 开始绘制x轴的数据
+        index = list(range(len(y)))
+        # name_list = ['a0', 'a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9']  # x轴标签
+        plt.xticks(index)  # 绘制x轴的标签
+
+        # 柱形图顶端数值显示
+        for data in first_bar:
+            y = data.get_height()
+            x = data.get_x()
+            plt.text(x + 0.15, y, str(y), va='bottom')  # 0.15为偏移值，可以自己调整，正好在柱形图顶部正中
+
+        # 图片的显示及存储
+        # plt.savefig("./static\\images\\draw\\hist.png")
+        src = Path('./static/images/draw/hist.png')
+        plt.savefig(src)
+        plt.close()
+        if os.path.exists('./static/images/draw/hist.png'):
+            print("success")
